0e8171425f2d4e7cb2cd309745593580f7c06ec9
dougsfelipe <dfcs@cin.ufpe.br>
2020-06-16 00:53:44 -0300

    Start deploy

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(path.join(__dirname, 'dist','app_name')));
 
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
 
 console.log('Console Listening');
